8c387412bb0f8f2d0a7c3f49549b292b2eeefe55
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2023-09-03 17:59:06 +0530

    added order function

---
+++ app.js
@@ -0,0 +1,27 @@
+import Store from "./services/Store.js";
+import API from "./services/API.js";
+import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
+// Link Web Components
+import { MenuPage } from "./components/MenuPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
+import OrderPage from "./components/OrderPage.js";
+import ProductItem from "./components/ProductItem.js";
+import CartItem from "./components/CartItem.js";
+window.app = {}
+app.store = Store;
+app.router = Router;
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadData()
+    app.router.init()
+})
+
+window.addEventListener("appcartchange", event => {
+    const badge = document.getElementById("badge")
+    const qty = app.store.cart.reduce(
+        (acc, item) => acc + item.quantity, 0
+    )
+    badge.textContent = qty
+    badge.hidden = qty == 0
+})
---
+++ services/Router.js
@@ -0,0 +1,49 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll("a.navlink").forEach(link => {
+            link.addEventListener("click", event => {
+                event.preventDefault();
+                const url = event.target.getAttribute("href")
+                Router.go(url)
+            })
+        })
+        //Event handler for URL changes
+        window.addEventListener("popstate", event => {
+            Router.go(event.state.route, false);
+        })
+        //Check the intial URL
+        Router.go(location.pathname);
+    },
+    go: (route, addToHistory=true) => {
+        console.log(`Going to ${route}`)
+
+        if(addToHistory) {
+            history.pushState({ route }, '', route)
+        }
+        let pageElement = null;
+        switch(route) {
+            case "/":
+                pageElement = document.createElement("menu-page")
+                break;
+            case "/order":
+                pageElement = document.createElement("order-page")
+                break;
+            default:
+                if(route.startsWith("/product-")) {
+                    pageElement = document.createElement("details-page");
+                    const paramId = route.substring(route.lastIndexOf("-")+1)
+                    pageElement.dataset.productId = paramId;
+                }
+        }
+        if(pageElement) {
+            // document.querySelector("main").children[0].remove()
+            const cache = document.querySelector("main") 
+            cache.innerHTML = "";
+            cache.appendChild(pageElement);
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+    }
+}
+
+export default Router;
